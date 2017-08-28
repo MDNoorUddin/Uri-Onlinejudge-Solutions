6d4aef3a4f91abbe348561101888a21006441e51
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 13:47:07 +0600

    Extremely Basic

---
+++ 1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=a+b;
+        System.out.println("X = "+c);
+    }
+}
