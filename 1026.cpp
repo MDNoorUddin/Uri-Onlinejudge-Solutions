e3e806fd5c156c162ee700fb63afb91ec4f83b61
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 16:02:50 +0600

    To Carry or not to Carry

---
+++ 1026.cpp
@@ -0,0 +1,13 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+     long long x,y;
+     while(cin>>x>>y)
+     {
+         long long z;
+         z=x^y;
+         cout<<z<<endl;
+     }
+     return 0;
+ }
