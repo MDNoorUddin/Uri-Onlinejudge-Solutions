8f2a87a0eddea9110c9ac2cc70302ea5745f9ce4
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 13:41:21 +0600

    I am Toorg!

---
+++ 2581.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      while(sc.hasNext()){
+         int number;
+         number=sc.nextInt();
+         sc.next();
+         while(number>0){
+           String id1;
+           id1=sc.nextLine();
+           number--;
+           System.out.println("I am Toorg!");
+         }
+     }
+   }
+}
+           
