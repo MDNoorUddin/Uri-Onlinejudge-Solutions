46d52271293a0dec14d294ebb844faf356dc373d
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 13:53:26 +0600

    Flíper

---
+++ 2454.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==0)
+        cout<<"C\n";
+    else
+    {
+        if(b==0)
+            cout<<"B\n";
+        else
+            cout<<"A\n";
+    }
+    return 0;
+}
