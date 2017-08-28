7e0c7d72a9d55f6ad9f18b6028529d803fa42f9e
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 16:00:44 +0600

    Merry Christmaaas!

---
+++ 2483.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"Feliz nat";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"a";
+    }
+    cout<<"l!\n";
+    return 0;
+}
