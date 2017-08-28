47727323c5aa90d03e3a99a739981799326e5d36
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-28 15:04:32 +0600

    Nove

---
+++ 2217.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n%2==0)
+        {
+            cout<<"1\n";
+        }
+        else
+        {
+            cout<<"9\n";
+        }
+    }
+    return 0;
+}
