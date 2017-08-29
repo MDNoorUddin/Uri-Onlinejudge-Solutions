c4809a1056858da168e71a0af99263c2fae6a8f8
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:42:19 +0600

    Brazil World Cup

---
+++ 1564.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+     int x;
+     while((scanf("%d",&x))!=EOF)
+     {
+         if(x>0)
+         cout<<"vai ter duas!\n";
+         else
+         cout<<"vai ter copa!\n";
+     }
+ 
+    return 0;
+}
