d4e69fb4234ee5c6d4748a2792025b7ec52e72b6
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 13:11:46 +0600

    Distance

---
+++ 1016.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int a;
+     cin>>a;
+     a=a*2;
+     cout<<a<<" minutos\n";
+ 
+    return 0;
+}
