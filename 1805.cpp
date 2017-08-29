4237569c7bb550475a516494c761904da93c836f
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 10:31:16 +0600

    Natural Sum

---
+++ 1805.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+
+	long long int soma = 0;
+	long long int a,b;
+
+	cin >> a >> b;
+
+	soma = (a + b) * (b-a+1)/2;
+
+	cout << soma << endl;
+
+
+}
