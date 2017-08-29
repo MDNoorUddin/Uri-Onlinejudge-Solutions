764f78b747d38e93b1a628095fedd5aae9a2ef0d
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:33:04 +0600

    Back to High School Physics

---
+++ 1197.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+
+     int a,b;
+     while((scanf("%d%d",&a,&b))!=EOF)
+     {
+         cout<<2*a*b<<endl;
+     }
+ 
+    return 0;
+}
