92dae117486807b343306f14101655001d13053f
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 12:40:38 +0600

    Odd Numbers

---
+++ 1067.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int X;
+    cin>>X;
+    for(int i=1;i<=X;i++)
+    {
+        if(i%2!=0)
+        cout<<i<<endl;
+    }
+ 
+    return 0;
+}
