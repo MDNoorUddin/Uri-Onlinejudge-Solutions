988f9ad35173aca050a10ee1b97319c46fde1919
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 13:14:35 +0600

    Salary

---
+++ 1008.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    float c;
+    cin>>a>>b>>c;
+    float d;
+    d=b*c;
+    cout<<"NUMBER = "<<a<<endl;
+    printf("SALARY = U$ %.2f\n",d);
+    return 0;
+}
