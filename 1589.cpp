219932c8f020c8a3a0a9882ba3774f1149587627
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:43:03 +0600

    Bob Conduit

---
+++ 1589.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,t;
+    cin>>t;
+    while(t>0)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+        t--;
+    }
+    return 0;
+}
