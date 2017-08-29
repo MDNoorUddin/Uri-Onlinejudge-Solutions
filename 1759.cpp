5a9a5201330b31eff73fd0e7eb7131486cb412fd
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:46:24 +0600

    Ho Ho Ho

---
+++ 1759.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+     int t;
+     cin>>t;
+     for(int i=1;i<=t;i++)
+     {
+         cout<<"Ho";
+         if(i==t)
+         cout<<"!\n";
+         else
+         cout<<" ";
+     }
+ 
+    return 0;
+}
