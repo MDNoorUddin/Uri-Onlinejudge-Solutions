183a2dad282ef2fbf13863f2910683a34a81dc02
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:49:38 +0600

    Our Days Are Never Coming Back

---
+++ 1864.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+ 
+int main() {
+ 
+    string s="LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int a;
+    cin>>a;
+    for(int i=0;i<a;i++)
+    cout<<s[i];
+    cout<<endl;
+ 
+    return 0;
+}
