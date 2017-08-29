3a0e3605fb6da7366e55c0c019ab22422fd2cb01
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 12:49:39 +0600

    Even Numbers

---
+++ 1059.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+     for(int i=2;i<=100;i++)
+     {
+         if(i%2==0)
+         {
+             cout<<i<<endl;
+         }
+     }
+ 
+    return 0;
+}
