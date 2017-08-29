41b1595306fca3e7943ceda1821e73e5faecbb7c
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 11:08:26 +0600

    Vitória and Her Indecision

---
+++ 1924.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin>>t;
+    while(t-->0)
+    {
+        string g;
+        cin>>g;
+    }
+    cout<<"Ciencia da Computacao\n";
+ 
+    return 0;
+}
