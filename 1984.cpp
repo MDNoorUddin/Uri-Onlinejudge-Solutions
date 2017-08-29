2d4582b39fc70ce73f1585956e2b594605f56bd2
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 10:58:21 +0600

    The Pronalância Puzzle

---
+++ 1984.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<string> 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     string str;
+     cin>>str;
+     for(int i=str.length()-1;i>=0;i--)
+     cout<<str[i];
+     cout<<endl;
+ 
+    return 0;
+}
