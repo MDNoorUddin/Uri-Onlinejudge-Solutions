c5b6a6ef1fdcbb30bc52f6450dd52fc53ee15ad4
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-08-29 12:29:03 +0600

    Multiplication Table

---
+++ 1078.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    cin>>N;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<" x "<<N<<" = "<<i*N<<endl;
+    }
+ 
+    return 0;
+}
