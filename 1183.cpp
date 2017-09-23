c283d6c9f4a42687c42d682ced359403f555cb78
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-09-23 13:05:24 +0600

    Above the Main Diagonal

---
+++ 1183.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a[12][12];
+    string c;
+    cin>>c;
+    float ad=0.0;
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<12;j++)
+        {
+            cin>>a[i][j];
+            if(j>i)
+                ad+=a[i][j];
+        }
+    }
+    if(c=="S")printf("%.1f\n",ad);
+    else printf("%.1f\n",ad/66);
+    return 0;
+}
