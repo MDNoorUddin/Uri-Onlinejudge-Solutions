c5d03e13da2ae88c36a0c739ef5cf08568aa40bc
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-10-14 23:01:48 +0600

    Feynman

---
+++ 1323.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n&&n)
+    {
+        long long x=(n*(n+1)*(2*n+1))/6;
+        cout<<x<<endl;
+    }
+    return 0;
+}
+    
