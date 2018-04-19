995ab620a257e46a2fdfa97956013d1fb51e56fe
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-19 11:58:14 +0600

    Output 6

---
+++ 2752.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	string str;
+	str="AMO FAZER EXERCICIO NO URI";
+	printf("<% s>\n",str.c_str());
+	printf("<% 30s>\n",str.c_str());
+	printf("<% .20s>\n",str.c_str());
+	printf("<% -20s>\n",str.c_str());
+	printf("<% -30s>\n",str.c_str());
+	printf("<% .30s>\n",str.c_str());
+	printf("<% 30.20s>\n",str.c_str());
+	printf("<% -30.20s>\n",str.c_str());
+	return 0;
+}
