4f9832474c868293d927214519a7a50570a73866
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-19 09:27:14 +0600

    Input and Output Reading and Skipping Names

---
+++ 2766.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	string str;
+	for(int i=1;i<=10;i++)
+	{
+		cin>>str;
+		if(i==3||i==7||i==9)cout<<str<<endl;
+	}
+}
