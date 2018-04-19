9384944ede0efa8fb01996e25b6ce3c49177f7b2
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-19 09:58:51 +0600

    CPF Input and Output

---
+++ 2763.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	string str;
+	getline(cin,str);
+	for(int i=0;i<str.length();i++)
+	{
+		if(str[i]>='0'&&str[i]<='9')cout<<str[i];
+		else cout<<endl;
+	}
+	cout<<endl;
+}
