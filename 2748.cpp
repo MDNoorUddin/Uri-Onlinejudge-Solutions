9862731d60feff275325870aed321cff8f45e8f3
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-19 11:08:23 +0600

    Output 2

---
+++ 2748.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	printf("---------------------------------------\n");
+	printf("|        Roberto                      |\n");
+	printf("|                                     |\n");
+	printf("|        5786                         |\n");
+	printf("|                                     |\n");
+	printf("|        UNIFEI                       |\n");
+	printf("---------------------------------------\n");
+}
