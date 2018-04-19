98d61b709420cd8471006ca3a5de3f55960cd443
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-19 12:12:35 +0600

    Output 10

---
+++ 2756.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+
+int main(int argc, char** argv) {
+	printf("       A\n");
+	printf("      B B\n");
+	printf("     C   C\n");
+	printf("    D     D\n");
+	printf("   E       E\n");
+	printf("    D     D\n");
+	printf("     C   C\n");
+	printf("      B B\n");
+	printf("       A\n");
+	return 0;
+}
