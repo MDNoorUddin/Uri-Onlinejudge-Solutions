7f183c5a55e28fb97293b6a931b77e366f3e4f2f
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2018-06-17 11:08:35 +0600

    Robot Basketball

---
+++ 2780.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef std::vector<double> vi;
+
+#define PI acos(-1)
+#define E 2.718281828459
+#define g 9.81
+
+int main(int argc, char** argv) {
+    int D;
+    cin>>D;
+     if(D >1400)
+    cout<<3<<endl;
+
+    else if(D>800)cout<<2<<endl;
+    else cout<<1<<endl;
+}
