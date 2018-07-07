8c852a0f94ab4ea29835172062c70aa7a5e197f6
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2018-07-07 21:17:15 +0600

    Bean

---
+++ 2791.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include<set>
+#include<string>
+#include<cstring>
+#include<cstdio>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include <utility>
+#include <sstream>
+#include<bitset>
+#include<string.h>
+
+using namespace std;
+
+typedef long long ll;
+
+
+#define PI acos(-1)
+#define E 2.718281828459
+#define g 9.81
+
+
+int main(int argc, char** argv) {
+
+    int n;
+    for(int i=1;i<=4;i++)
+    {
+        cin>>n;
+        if(n==1)cout<<i<<endl;
+    }
+
+	return 0;
+}
