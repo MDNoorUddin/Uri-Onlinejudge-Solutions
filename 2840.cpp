bdd4b25597d00e883342ec876eb7a57861889fae
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2019-04-21 10:28:58 +0600

    Balloon++

---
+++ 2840.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define INF 99999999
+int main()
+{
+    double a,b;
+    cin>>a>>b;
+    double r=pow(a,3);
+    a=  1.333333333 * (3.1415*r);
+    int g=b/a;
+    cout<<g<<'\n';
+}
