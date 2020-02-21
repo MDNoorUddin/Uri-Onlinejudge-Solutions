0d02477f197e53cf78211e7e920f66f79dc69706
MD. Noor Uddin <nooruddinneon16321@gmail.com>
2020-02-21 23:43:57 +0600

    Balloon of Honor

---
+++ 2987.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     char c;
+     
+     cin>>c;
+     cout<<c-64<<endl;
+ 
+    return 0;
+}
